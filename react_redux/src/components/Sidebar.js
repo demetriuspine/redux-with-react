0ddcd3030c8802a5d701ceab455c739f9480a145
lucas <mr.pine97@gmail.com>
2021-10-12 00:43:40 -0300

    Sidebar refatorado com o redux

--- react_redux/src/components/Sidebar.js
+++ react_redux/src/components/Sidebar.js
@@ -4,12 +4,12 @@ import { connect } from 'react-redux';
 class Sidebar extends React.Component {
 
   render() {
-    const { categories } = this.props;
+    const { categoriesInGlobalState } = this.props; // a chave categoriesInGlobalState do mapStateToProps, vira uma prop
 
     return (
       <aside>
         {
-          categories.map(category => (
+          categoriesInGlobalState.map(category => (
             <div key={category.id}>
               <h3>{category.name}</h3>
               <ul>
@@ -27,4 +27,10 @@ class Sidebar extends React.Component {
   }
 }
 
-export default connect(mapStateToProps(), mapDispatchToProps())(Sidebar)
+function mapStateToProps(state) { //recebe como parâmetro o estado global
+  return {
+    categoriesInGlobalState: state.categories // aqui eu crio um nome da chave, e recebo o estado que faz sentido pra mim
+  }
+}
+
+export default connect(mapStateToProps)(Sidebar)
