1769619bc9b0d318e36766aff3911a41767bb44f
lucas <mr.pine97@gmail.com>
2021-10-12 00:28:08 -0300

    limpando o App.js

--- react_redux/src/App.js
+++ react_redux/src/App.js
@@ -3,19 +3,10 @@ import { Provider } from 'react-redux';
 import './App.css';
 import Player from './components/Player';
 import Sidebar from './components/Sidebar';
-import categories from './data';
 import store from './store';
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      categories: categories
-    }
-  }
   render(){
-    const { categories } = this.state;
     return (
       <div className="App">
         <Provider store={ store }>
@@ -24,7 +15,7 @@ class App extends React.Component {
           </header>
           <main className="App-main">
             <Player />
-            <Sidebar categories={ categories } />
+            <Sidebar />
           </main>
         </Provider>
       </div>
