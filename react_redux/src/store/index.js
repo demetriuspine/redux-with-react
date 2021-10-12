d60f31ce72ae473eb7fdbeb94134f7b86c123e71
lucas <mr.pine97@gmail.com>
2021-10-12 00:54:06 -0300

    snippets

--- react_redux/src/store/index.js
+++ react_redux/src/store/index.js
@@ -4,3 +4,32 @@ import movieReducer from "../reducers/movieReducer";
 const store = createStore(movieReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
+
+/*
+redux snipppets
+
+rxaction→
+export const actionName = (payload) => ({
+  type: type,
+  payload
+})
+
+rxreducer→
+const initialState = {
+
+}
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+
+  case typeName:
+    return { ...state, ...payload }
+
+  default:
+    return state
+  }
+}
+
+
+
+
+*/
