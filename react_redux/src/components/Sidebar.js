d1a631d6aba25c9c292ed50390cae5d349e68839
lucas <mr.pine97@gmail.com>
2021-10-12 00:46:48 -0300

    outro jeito de fazer

--- react_redux/src/components/Sidebar.js
+++ react_redux/src/components/Sidebar.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 import { connect } from 'react-redux';
 
-class Sidebar extends React.Component {
+export default connect(mapStateToProps)(class Sidebar extends React.Component {
 
   render() {
     const { categoriesInGlobalState } = this.props; // a chave categoriesInGlobalState do mapStateToProps, vira uma prop
@@ -25,12 +25,10 @@ class Sidebar extends React.Component {
       </aside>
     )
   }
-}
+})
 
 function mapStateToProps(state) { //recebe como parâmetro o estado global
   return {
     categoriesInGlobalState: state.categories // aqui eu crio um nome da chave, e recebo o estado que faz sentido pra mim
   }
 }
-
-export default connect(mapStateToProps)(Sidebar)
